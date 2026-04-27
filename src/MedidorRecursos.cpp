07c253196eb82f4b6400710bc226a52e61edaf0e
dortiz02 <dortiz.romero@udea.edu.co>
2026-04-27 00:11:45 -0500

    Update MedidorRecursos.cpp
    final verify

--- src/MedidorRecursos.cpp
+++ src/MedidorRecursos.cpp
@@ -1,26 +1,18 @@
 #include "../include/MedidorRecursos.h"
 #include <iostream>
 
-// -- Constructores ----------------------------------------------------
-
-MedidorRecursos::MedidorRecursos()
-    : iteraciones(0), memoriaBytes(0) {}
+MedidorRecursos::MedidorRecursos() : iteraciones(0), memoriaBytes(0) {}
 
 MedidorRecursos::MedidorRecursos(const MedidorRecursos& otro)
-    : iteraciones (otro.iteraciones),
-      memoriaBytes(otro.memoriaBytes) {}
+    : iteraciones(otro.iteraciones), memoriaBytes(otro.memoriaBytes) {}
 
 MedidorRecursos::~MedidorRecursos() {}
-
-// -- Getters ----------------------------------------------------------
 
 long long MedidorRecursos::getIteraciones()  const { return iteraciones;  }
 size_t    MedidorRecursos::getMemoriaBytes() const { return memoriaBytes; }
 
-// -- Logica -----------------------------------------------------------
-
-/// Incrementa el contador de iteraciones en n unidades
-/// Se llama desde cualquier bucle o llamada recursiva del sistema
+/// Incrementa el contador en n unidades
+/// Se llama desde cualquier bucle o llamada del sistema
 void MedidorRecursos::incrementar(long long n) {
     iteraciones += n;
 }
@@ -33,48 +25,40 @@ void MedidorRecursos::resetear() {
 
 /// Calcula el total de memoria de todas las estructuras activas
 /// Suma el tamano de cada clase multiplicado por su cantidad
-/// Incluye objetos en heap y estimacion de variables locales relevantes
+/// Incluye estimacion de strings y overhead de listas dinamicas
 void MedidorRecursos::calcularMemoria(
-    int numEquipos,
-    int jugadoresPorEquipo,
-    int numGrupos,
-    int partidosPorGrupo,
-    int numEtapas,
-    int partidosPorEtapa)
+    int numEquipos, int jugadoresPorEquipo,
+    int numGrupos,  int partidosPorGrupo,
+    int numEtapas,  int partidosPorEtapa)
 {
     memoriaBytes = 0;
 
-    // Equipos y sus estadisticas
-    memoriaBytes += sizeof(double) * 4;             // EstadisticasEquipo (8 ints)
-    memoriaBytes += (sizeof(double) * 8             // EstadisticasEquipo
-                  +  sizeof(double) * 7             // EstadisticasJugador
-                  +  sizeof(int)    * 3             // campos basicos Jugador
-                  +  64)                            // strings nombre/apellido estimado
+    // Jugadores: stats + campos basicos + strings estimados
+    memoriaBytes += (sizeof(int) * 7          // EstadisticasJugador
+                  +  sizeof(int) * 3          // numeroCamiseta + overhead
+                  +  64)                      // nombre + apellido estimado
                   * jugadoresPorEquipo * numEquipos;
 
-    // Equipos
-    memoriaBytes += (sizeof(int) + 64 * 3)          // rankingFIFA + strings pais/conf/dt
+    // Equipos: stats + campos + strings
+    memoriaBytes += (sizeof(int) * 8          // EstadisticasEquipo
+                  +  sizeof(int)              // rankingFIFA
+                  +  64 * 3)                  // pais + confederacion + dt estimados
                   * numEquipos;
 
-    // Grupos
-    memoriaBytes += sizeof(int) * 4                 // puntos[4]
-                  * numGrupos;
+    // Grupos: puntos
+    memoriaBytes += sizeof(int) * 4 * numGrupos;
 
-    // Partidos de fase de grupos
-    memoriaBytes += (sizeof(bool) * 2               // flags partido
-                  +  64 * 5                         // strings fecha/hora/sede/arbitros
-                  +  sizeof(ActaEquipo) * 2         // dos actas por partido
-                  +  sizeof(ActaJugador) * 11 * 2)  // 11 jugadores por acta
-                  * partidosPorGrupo * numGrupos;
+    // Partidos fase grupos
+    size_t tamPartido = sizeof(bool) * 2
+                      + 64 * 5               // strings fecha/hora/sede/arbitros
+                      + sizeof(ActaEquipo) * 2
+                      + sizeof(ActaJugador) * 11 * 2;
+    memoriaBytes += tamPartido * partidosPorGrupo * numGrupos;
 
-    // Partidos de etapas eliminatorias
-    memoriaBytes += (sizeof(bool) * 2
-                  +  64 * 5
-                  +  sizeof(ActaEquipo) * 2
-                  +  sizeof(ActaJugador) * 11 * 2)
-                  * partidosPorEtapa * numEtapas;
+    // Partidos etapas eliminatorias
+    memoriaBytes += tamPartido * partidosPorEtapa * numEtapas;
 
-    // Listas dinamicas (overhead de puntero + tam + cap)
+    // Overhead de listas dinamicas (puntero datos + tam + cap)
     memoriaBytes += sizeof(void*) * 3
                   * (numEquipos + numGrupos + numEtapas);
 }
@@ -82,22 +66,22 @@ void MedidorRecursos::calcularMemoria(
 /// Imprime el reporte de la funcionalidad recien ejecutada
 void MedidorRecursos::reportar(const std::string& nombreFuncionalidad) const {
     std::cout << "\n--- Reporte de recursos: " << nombreFuncionalidad << " ---\n";
-    std::cout << "Iteraciones:    " << iteraciones  << "\n";
-    std::cout << "Memoria total:  " << memoriaBytes << " bytes ("
+    std::cout << "Iteraciones:   " << iteraciones  << "\n";
+    std::cout << "Memoria total: " << memoriaBytes << " bytes ("
               << memoriaBytes / 1024.0 << " KB)\n";
     std::cout << "---------------------------------------------\n";
 }
 
 /// Suma dos medidores para acumular reportes parciales
 MedidorRecursos MedidorRecursos::operator+(const MedidorRecursos& otro) const {
-    MedidorRecursos resultado;
-    resultado.iteraciones  = iteraciones  + otro.iteraciones;
-    resultado.memoriaBytes = memoriaBytes + otro.memoriaBytes;
-    return resultado;
+    MedidorRecursos r;
+    r.iteraciones  = iteraciones  + otro.iteraciones;
+    r.memoriaBytes = memoriaBytes + otro.memoriaBytes;
+    return r;
 }
 
 std::ostream& operator<<(std::ostream& os, const MedidorRecursos& m) {
     os << "Iteraciones: " << m.iteraciones
-       << " | Memoria: " << m.memoriaBytes << " bytes";
+       << " | Memoria: "  << m.memoriaBytes << " bytes";
     return os;
 }
